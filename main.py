1637753f3affba96a75d4aca8817a3665a69b382
Lomelo <lomeloseugasala@outlook.com>
2025-01-18 17:51:03 -0700

    created asteroidfield, collision with player, game over, and some adjustments to settings

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 from constants import *
 
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -10,15 +12,36 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = updatable
+    asteroidfield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for fun in updatable:
+            fun.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.collide(player):
+                print("Game over!")
+                return
+
+
+        for fun in drawable:
+            fun.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
